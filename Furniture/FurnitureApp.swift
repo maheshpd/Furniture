75c2d2ec6df4c3ced0520b16a96bbc3a2b817fd4
maheshprasad <mahesh223prasad@gmail.com>
2022-04-17 14:44:54 +0530

    Initial Commit

---
+++ Furniture/FurnitureApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureApp.swift
+//  Furniture
+//
+//  Created by Mahesh Prasad on 17/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
